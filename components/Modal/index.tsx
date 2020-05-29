b56ed2006961fa0add3f715f0f8221c97e3c2932
robinyang11 <756517553@qq.com>
2020-05-30 05:41:48 +0800

    feat:add basemodal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -18,6 +18,126 @@ interface IModalProps {
   closeText?: string;
 }
 
+function BaseModal(props: IModalProps) {
+
+  const [visible, setVisible] = useState(props.visible);
+  useEffect(() => {
+    setVisible(props.visible);
+  }, [props.visible]);
+
+  function closeModal() {
+
+    if (props.onClose) {
+      props.onClose();
+    } else {
+      setVisible(false);
+    }
+
+  }
+
+  function renderCloseButton() {
+    if (props.isCloseButtonVisible === undefined ||
+      props.isCloseButtonVisible === true) {
+      return (<span
+        onClick={() => {
+          closeModal();
+        }}
+        className="close-btn"
+      >
+        <Icon
+          type="tishi"
+        />
+      </span>);
+    }
+    return null;
+  }
+
+  /**
+   * when modal show ,set body unscrollable!
+   */
+  document.body.style.overflow = props.visible ? 'hidden' : 'auto';
+
+  return (
+    <div className="modal">
+      {visible ? <div
+        onClick={(e) => {
+          closeModal();
+        }}
+        className="mask"
+      >
+      </div> : ''}
+      <CSSTransition
+        in={visible}
+        classNames="alert"
+        timeout={300}
+        unmountOnExit
+        onEnter={() => {
+          console.log("onEnter")
+        }}
+        onExit={() => {
+          closeModal();
+        }}
+      >
+        <div
+          onClick={(e) => {
+            if (e.target === e.currentTarget) {
+              e.stopPropagation();
+              closeModal();
+            }
+          }}
+          role="document" className="content-wrapper">
+          <div className="content"
+            style={{
+              width: props.width || "500px",
+              padding: "10px 0",
+              ...props.style,
+            }}
+          >
+            {
+              props.children
+            }
+          </div>
+        </div>
+      </CSSTransition>
+    </div>
+  )
+}
+
+const modalDom = props => (
+  <div>
+    <div className="modal-header">
+      <h4>{props.title}</h4>
+      {
+        props.renderCloseButton()
+      }
+    </div>
+    <div className="modal-body">
+      {
+        props.children
+      }
+    </div>
+    <div className="modal-footer">
+      {
+        props.footer !== undefined ? props.footer : (
+          <React.Fragment>
+            <Button
+              type="default"
+              onClick={() => {
+                props.closeModal();
+              }}
+              style={{ marginRight: "10px" }}
+            >
+              {props.closeText || "cancel"}
+            </Button>
+            <Button type="primary">{props.sureText || "ok"}</Button>
+          </React.Fragment>
+        )
+      }
+    </div>
+  </div>
+)
+
+
 function Modal(props: IModalProps) {
 
   const [visible, setVisible] = useState(props.visible);
